9e2f181ba50e4a36d928521342ffa2634438e077
Julian vD <Julian vD>
2017-04-04 19:14:18 +0200

    Fixed issue regarding crashed caused by 'pq->a[pq->tail] = data;'

--- nav.h
+++ nav.h
@@ -1,26 +1,25 @@
 #include "queue.h"
 
 enum DIRECTION {
-	FORWARD = 0,
-	LEFT = 1,
-	RIGHT = 2,
-	TURNABOUT = 3
+	NONE = 0,
+	FORWARD = 1,
+	LEFT = 2,
+	RIGHT = 3,
+	TURNABOUT = 4
 };
 
-Queue* nav_queue;
-
 void init_nav_queue() {
-	init_queue(nav_queue);
+	init_queue(&nav_queue);
 }
 
 void enqueue_direction(DIRECTION dir) {
-	enqueue(nav_queue, dir);
+	enqueue(&nav_queue, dir);
 }
 
 DIRECTION dequeue_direction() {
-	return (DIRECTION) dequeue(nav_queue);
+	return (DIRECTION) dequeue(&nav_queue);
 }
 
-void resolveJunction() {
-	dequeue(nav_queue);
+DIRECTION peek_direction() {
+	return (DIRECTION) peek(&nav_queue);
 }
--- queue.h
+++ queue.h
@@ -4,43 +4,58 @@ struct Queue {
 		int a[QUEUESIZE];
 		int head;
 		int tail;
-		int items;
+		int item_count;
 };
 
-void init_queue(Queue* pq) {
+Queue nav_queue;
+
+void empty_queue(Queue* pq) {
 	pq->head = 0;
 	pq->tail = 0;
-	pq->items = 0;
+	pq->item_count = 0;
+}
+
+void init_queue(Queue* pq) {
+	empty_queue(pq);
 }
 
 void enqueue(Queue* pq, int data) {
-	if (pq->items == QUEUESIZE) {
+	if (pq->item_count == QUEUESIZE - 1) {
 		return;
 	}
 
-	pq->a[pq->tail] = data;
-	pq->tail--;
+	pq->tail++;
 
-	if (pq->tail == -1) {
-		pq->tail = QUEUESIZE - 1;
+	nav_queue.a[pq->tail] = data;
+
+	if (pq->tail == QUEUESIZE - 1) {
+		pq->tail = 0;
 	}
 
-	pq->items++;
+	pq->item_count++;
 }
 
 int dequeue(Queue* pq) {
-	if (pq->items == 0) {
+	if (pq->item_count == 0) {
 		return 0;
 	}
 
+	pq->head++;
 	int data = pq->a[pq->head];
-	pq->head--;
 
-	if (pq->head == -1) {
-		pq->head = QUEUESIZE - 1;
+	if (pq->head == QUEUESIZE - 1) {
+		pq->head = 0;
 	}
 
-	pq->items--;
+	pq->item_count--;
 
 	return data;
+}
+
+int peek(Queue* pq) {
+	if (pq->item_count == 0) {
+		return 0;
+	}
+
+	return pq->a[pq->head];
 }
