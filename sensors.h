1f67ecc10b12451db674fb4e45546efd6b686e88
Julian vD <Julian vD>
2017-04-04 19:24:41 +0200

    Added stop_speed_multiplier and changed POWER to 65

--- sensors.h
+++ sensors.h
@@ -1,5 +1,5 @@
 #define WHITE_VALUE 64
-#define POWER 75
+#define POWER 65
 
 int get_sonar() {
 	return SensorValue(SONAR);
@@ -13,18 +13,18 @@ int get_light_right() {
 	return SensorValue(LIGHT_R);
 }
 
-int get_power_left() {
+int get_power_left(float stop_speed_multiplier = 1.0) {
 	int light_val_l = get_light_left();
 	int error_l = light_val_l - WHITE_VALUE;
-	int power_left = POWER + (error_l * 200 / WHITE_VALUE);
+	int power_left = POWER * stop_speed_multiplier + (error_l * 200 / WHITE_VALUE);
 
 	return power_left;
 }
 
-int get_power_right() {
+int get_power_right(float stop_speed_multiplier = 1.0) {
 	int light_val_r = get_light_right();
 	int error_r = light_val_r - WHITE_VALUE;
-	int power_right = POWER + (error_r * 200 / WHITE_VALUE);
+	int power_right = POWER * stop_speed_multiplier + (error_r * 200 / WHITE_VALUE);
 
 	return power_right;
 }
