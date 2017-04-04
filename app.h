a7439cac2b97a065396cc072687f3a0615430b1f
Julian vD <Julian vD>
2017-04-04 19:09:45 +0200

    Added smooth start/stop mechanism for app

--- app.h
+++ app.h
@@ -1,22 +1,56 @@
-#include "nav.h"
-
 long nLastXmitTimeStamp = nPgmTime;
 long nDeltaTime         = 0;
+
+bool changing_speed = false;
+bool force_stopped = false;
+float stop_speed_multiplier = 1.0;
 
 const int kMaxSizeOfMessage = 30;
 const int INBOX = 5;
 
+void toggle_stop_robot() {
+	if (!changing_speed) {
+		force_stopped = !force_stopped;
+	}
+}
+
 void handle_message(string msg) {
-//	writeDebugStreamLine("%s", msg);
-
 	if (msg == "UP") {
-
+		enqueue_direction(FORWARD);
 	} else if (msg == "DOWN") {
 		enqueue_direction(TURNABOUT);
 	} else if (msg == "LEFT") {
 		enqueue_direction(LEFT);
 	} else if (msg == "RIGHT") {
 		enqueue_direction(RIGHT);
+	} else if (msg == "FIRE") {
+		toggle_stop_robot();
+	} else if (msg == "A") {
+		empty_queue(nav_queue);
+	}
+}
+
+task start_stop_task() {
+	while (1) {
+		waitUntil(force_stopped);
+		changing_speed = true;
+		while (force_stopped && stop_speed_multiplier > 0) {
+			stop_speed_multiplier -= 0.02;
+			wait1Msec(10);
+		}
+		changing_speed = false;
+
+		stop_speed_multiplier = 0;
+
+		waitUntil(!force_stopped);
+		changing_speed = true;
+		while (!force_stopped && stop_speed_multiplier < 1) {
+			stop_speed_multiplier += 0.02;
+			wait1Msec(10);
+		}
+		changing_speed = false;
+
+		stop_speed_multiplier = 1;
 	}
 }
 
@@ -47,4 +81,5 @@ task app_task() {
 
 void init_app() {
 	startTask(app_task);
+	startTask(start_stop_task);
 }
